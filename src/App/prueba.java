0fe8c9c7665b6a880d2a491c8b04bd6a66967ad4
esteb <esteb@TUFesteban>
2024-06-14 17:13:31 -0500

    prueba esteban

--- src/App/prueba.java
+++ src/App/prueba.java
@@ -10,6 +10,6 @@ package App;
  */
 public class prueba {
     public static void main(String[] args) {
-        System.out.println("esteban");
+        System.out.println("funciona por favor");
     }
 }
