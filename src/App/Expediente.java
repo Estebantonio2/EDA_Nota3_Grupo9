3cf51f4f6e05b95330c26d14555574013e16feca
esteb <esteb@TUFesteban>
2024-06-29 02:11:51 -0500

    clase Expediente, clase Dependencia, mejora clase Interesado y prueba

--- src/App/Expediente.java
+++ src/App/Expediente.java
@@ -3,6 +3,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package App;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 /**
  *
@@ -12,13 +14,24 @@ public class Expediente {
     private String id, asunto, documentoRef;
     private int prioridad;
     private Interesado interesado;
+    private Dependencia dependencia;
+    private LocalDateTime tiempoInicio;
+    private LocalDateTime tiempoFin;
 
-    public Expediente(String id, String asunto, String documentoRef, int prioridad, Interesado interesado) {
+    public Expediente(Dependencia dependencia) {
+        this.dependencia = dependencia;
+        this.tiempoInicio = LocalDateTime.now();
+    }
+    
+    public Expediente(String id, String asunto, String documentoRef, int prioridad, Interesado interesado, Dependencia dependencia) {
         this.id = id;
         this.asunto = asunto;
         this.documentoRef = documentoRef;
         this.prioridad = prioridad;
         this.interesado = interesado;
+        this.dependencia = dependencia;
+        this.tiempoInicio = LocalDateTime.now();
+        
     }
 
     public String getId() {
@@ -60,6 +73,56 @@ public class Expediente {
     public void setInteresado(Interesado interesado) {
         this.interesado = interesado;
     }
+
+    public Dependencia getDependencia() {
+        return dependencia;
+    }
+
+    public void setDependencia(Dependencia dependencia) {
+        this.dependencia = dependencia;
+    }
+
+    public LocalDateTime getTiempoInicio() {
+        return tiempoInicio;
+    }
+
+    public void setTiempoInicio(LocalDateTime tiempoInicio) {
+        this.tiempoInicio = tiempoInicio;
+    }
+
+    public LocalDateTime getTiempoFin() {
+        return tiempoFin;
+    }
+
+    public void setTiempoFin(LocalDateTime tiempoFin) {
+        this.tiempoFin = tiempoFin;
+    }
     
+    public String getFechaInicio(){
+        DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        String fechaInicio = this.tiempoInicio.format(formato);
+        return fechaInicio;
+    }
     
+    public String getHoraInicio(){
+        DateTimeFormatter formato = DateTimeFormatter.ofPattern("HH:mm");
+        String horaInicio = this.tiempoInicio.format(formato);
+        return horaInicio;
+    }
+    
+    public void finExpediente(){
+        this.tiempoFin = LocalDateTime.now();
+    }
+    
+    public String getFechaFin(){
+        DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        String fechaFin = this.tiempoFin.format(formato);
+        return fechaFin;
+    }
+    
+    public String getHoraFin(){
+        DateTimeFormatter formato = DateTimeFormatter.ofPattern("HH:mm");
+        String horaFin = this.tiempoFin.format(formato);
+        return horaFin;
+    }
 }
