9071eb0023acde7f52ab01145cdf86cad683827b
Sistema <Sistema@192.168.1.50>
2024-06-28 22:47:21 -0500

    Clase administrador

---
+++ src/App/Administrador.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package App;
+
+/**
+ *
+ * @author Sistema
+ */
+public class Administrador {
+    private String nombreUsuario, contraseña;
+
+    public Administrador(String nombreUsuario, String contraseña) {
+        this.nombreUsuario = nombreUsuario;
+        this.contraseña = contraseña;
+    }
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+    
+    public void expediente(){
+        
+    }
+    
+    public void registrarMovimientoExpediente(){
+        
+    }
+    
+    public void finalizacionTramite(){
+        
+    }
+    
+    public String mostrarEstadoDeTramite(){
+        
+    }
+}
