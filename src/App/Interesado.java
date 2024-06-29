12521b8d897a85ed092170b89e4669bfbcdc0619
esteb <esteb@TUFesteban>
2024-06-28 22:15:28 -0500

    clase interesado

---
+++ src/App/Interesado.java
@@ -0,0 +1,63 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package App;
+
+/**
+ *
+ * @author esteb
+ */
+public class Interesado {
+    private String DNI, nombre, telefono, email, tipo;
+
+    public Interesado(String DNI, String nombre, String telefono, String email, String tipo) {
+        this.DNI = DNI;
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.email = email;
+        this.tipo = tipo;
+    }
+
+    public String getDNI() {
+        return DNI;
+    }
+
+    public void setDNI(String DNI) {
+        this.DNI = DNI;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+    
+    
+}
